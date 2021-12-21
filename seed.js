2a201afdf5b7246233318a930eff376de4ca018b
Michael Metcalf <m.metcalf6453@gmail.com>
2021-12-20 18:06:08 -0800

    added notes and consolidated some code

--- seed.js
+++ seed.js
@@ -8,8 +8,12 @@ const Survey = require('./SurveyModel.js');
 async function seed() {
     mongoose.connect(process.env.MONGO_DB);
 
-    await Survey.creat ({
-
+    await Survey.create ({
+        ClassNumber: 'Unknown Class',
+        ClassCount: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
+        Instructor: 'Unknown Instructor',
+        Survey_URL: '', //url
+        Created_On: Date,
     })
 
     mongoose.disconnect();
