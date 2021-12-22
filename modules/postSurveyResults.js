1890f338c4af894ad40b7786a788cca2b13de17a
Michael Metcalf <m.metcalf6453@gmail.com>
2021-12-22 14:05:53 -0800

    added delete form from JotForm when you post to DB

--- modules/postSurveyResults.js
+++ modules/postSurveyResults.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const axios = require('axios');
 // const verifyUser = require('../auth');
 const Survey = require('./SurveyModel');
 
 async function handlePostSurveyResults(req, res) {
+    if(req.body.surveyid === 'template') {
+        console.log('Unauthorized');
+    } else {
+    const formID = req.body.surveyid;
+    const apiKey = process.env.JOTFORM_API;
+    const url = `https://api.jotform.com/form/${formID}?apiKey=${apiKey}`;
+
     //   verifyUser(req, async (err, user) => {
     //     if (err) {
     //       console.error(err);
@@ -11,10 +19,13 @@ async function handlePostSurveyResults(req, res) {
     //     } else {
     try {
         const newSurvey = await Survey.create({ ...req.body });
-        res.status(201).send("New Survey Answers");
+        newSurvey.then(await axios.delete(url));
+        console.log('JotForm url is ', url);
+        res.status(201).send("New survey results moved from JotForm to the database")
     } catch (e) {
         console.error(e);
         res.status(500).send("Server Error");
+    }
     }
 }
 //   });
