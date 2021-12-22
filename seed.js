a488f0522d28f3d6a0e2a0d6f119584fb62b8347
Michael Metcalf <m.metcalf6453@gmail.com>
2021-12-21 16:33:19 -0800

    made sure Get function is working properly

--- seed.js
+++ seed.js
@@ -9,11 +9,34 @@ async function seed() {
     mongoose.connect(process.env.MONGO_DB);
 
     await Survey.create ({
-        ClassNumber: 'Unknown Class',
-        ClassCount: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
-        Instructor: 'Unknown Instructor',
-        Survey_URL: '', //url
-        Created_On: Date,
+        SurveyID: '000005',
+        CreatedOn: '2021-12-21 12:50:47',
+        SubmissionCount: 9,
+        Results: [0,0,0,0,1,2,0,0,3,0,2,0,0,1,0,0,0,0,0,0,0],
+    })
+    await Survey.create ({
+        SurveyID: '000004',
+        CreatedOn: '2021-12-19 12:03:41',
+        SubmissionCount: 12,
+        Results: [0,0,0,0,1,2,0,2,3,1,2,0,0,1,0,0,0,0,0,0,0],
+    })
+    await Survey.create ({
+        SurveyID: '000003',
+        CreatedOn: '2021-12-18 11:21:32',
+        SubmissionCount: 15,
+        Results: [0,0,0,0,1,2,0,0,6,2,3,0,0,1,0,0,0,0,0,0,0],
+    })
+    await Survey.create ({
+        SurveyID: '000002',
+        CreatedOn: '2021-12-17 11:01:55',
+        SubmissionCount: 9,
+        Results: [1,0,0,0,0,2,0,0,3,0,2,0,0,0,0,0,0,0,0,0,1],
+    })
+    await Survey.create ({
+        SurveyID: '000001',
+        CreatedOn: '2021-12-16 10:10:10',
+        SubmissionCount: 5,
+        Results: [0,0,0,0,0,0,0,0,3,0,2,0,0,0,0,0,0,0,0,0,0],
     })
 
     mongoose.disconnect();
