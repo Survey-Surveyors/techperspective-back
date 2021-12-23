20457a9aaf6840530dbaa62d55cab55f77233680
erik savage <erik.k.savage@gmail.com>
2021-12-23 13:17:46 -0800

    adds mongo _id to object response

--- modules/getActiveSurvey.js
+++ modules/getActiveSurvey.js
@@ -34,6 +34,7 @@ async function handleGetActiveSurvey(req, res) {
             }
 
             const surveyData = {
+                _id: activeSurvey._id,
                 surveyID: result.data.content[0].form_id, // 213494408669063 url can be built in front end
                 createdOn: result.data.content[0].created_at.split(' ')[0], //date survey was created
                 submissionCount: result.data.resultSet.count, // count of total survey submissions
@@ -44,6 +45,7 @@ async function handleGetActiveSurvey(req, res) {
 
         } else {
             const surveyData = {
+                _id: activeSurvey._id,
                 surveyID: formID,
                 createdOn: String(new Date()).split(' ').splice(1, 3).join('-'),
                 submissionCount: 0,
