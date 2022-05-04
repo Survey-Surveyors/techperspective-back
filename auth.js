fbec79f66ce4f7c9c1161be17496e310be9d08e0
Liesl White <lieslfwhite@gmail.com>
2022-05-04 16:11:04 -0700

    ownerID issue finally fixed

--- auth.js
+++ auth.js
@@ -3,31 +3,27 @@ const jwksClient = require('jwks-rsa');
 const jwt_decode = require('jwt-decode');
 
 const client = jwksClient({
-    jwksUri: process.env.JWKS_URI
+  jwksUri: process.env.JWKS_URI
 });
 
 function getKey(header, callback) {
-    client.getSigningKey(header.kid, function (err, key) {
-        console.error(err);
-        const signingKey = key.publicKey || key.rsaPublicKey;
-        callback(null, signingKey);
-    });
+  client.getSigningKey(header.kid, function (err, key) {
+    console.error(err);
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
 }
 
-// function getUser() {
-
-// }
-
 function verifyUser(req, errFirstOrUserCallbackFunction) {
-    try{
-        const token = req.headers.authorization.split(' ')[1];
-        console.log(token);
-        jwt.verify(token, getKey, {}, errFirstOrUserCallbackFunction);
-        let decoded = jwt_decode(token);
-        console.log('Decode Token: ', decoded);
-    } catch (error) {
-        errFirstOrUserCallbackFunction('Authorized Personnel Only, bye felicia!');
-    }
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    console.log(token);
+    jwt.verify(token, getKey, {}, errFirstOrUserCallbackFunction);
+    let decoded = jwt_decode(token);
+    console.log('Decode Token: ', decoded);
+  } catch (error) {
+    errFirstOrUserCallbackFunction('Authorized Personnel Only, bye felicia!');
+  }
 }
 
 module.exports = verifyUser;
--- modules/getActiveSurvey.js
+++ modules/getActiveSurvey.js
@@ -5,12 +5,18 @@ const axios = require('axios');
 const verifyUser = require('../auth');
 
 async function handleGetActiveSurvey(req, res) {
-    try {
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send('Invalid Token');
+    } else {
+
+      try {
         console.log('entered handleGetActiveSurvey.');
         const activeSurvey = await Survey.findOne({ active: true });
         // If there is no active survey send 204 and escape.
         if (activeSurvey === null) {
-            return res.status(204);
+          return res.status(204);
         }
 
         // Active survey exsists, GET ths survey information from JotForm using the survey ID.
@@ -21,47 +27,51 @@ async function handleGetActiveSurvey(req, res) {
 
         // If the survey has submissions, parse through result object to pull out relevent information
         if (result.data.content.length > 0) {
-            const surveyResponseArr = result.data.content.map(userReponseObj => Object.entries(userReponseObj.answers));
-            const surveyTrueCountArr = surveyResponseArr.map(answerArr => {
-                return answerArr.reduce((cntTrue, curVal) => {
-                    return cntTrue + (curVal[1].answer === 'TRUE' ? 1 : 0);
-                }, 0);
-            });
+          const surveyResponseArr = result.data.content.map(userReponseObj => Object.entries(userReponseObj.answers));
+          const surveyTrueCountArr = surveyResponseArr.map(answerArr => {
+            return answerArr.reduce((cntTrue, curVal) => {
+              return cntTrue + (curVal[1].answer === 'TRUE' ? 1 : 0);
+            }, 0);
+          });
 
-            const surveyResults = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+          const surveyResults = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-            for (let i = 0; i < surveyTrueCountArr.length; i++) {
-                surveyResults[surveyTrueCountArr[i] - 1]++;
-            }
+          for (let i = 0; i < surveyTrueCountArr.length; i++) {
+            surveyResults[surveyTrueCountArr[i] - 1]++;
+          }
 
-            const surveyData = {
-                _id: activeSurvey._id,
-                surveyID: result.data.content[0].form_id, // 213494408669063 url can be built in front end
-                createdOn: result.data.content[0].created_at.split(' ')[0], //date survey was created
-                submissionCount: result.data.resultSet.count, // count of total survey submissions
-                results: surveyResults, //array of total true counts
-            }
+          const surveyData = {
+            _id: activeSurvey._id,
+            surveyID: result.data.content[0].form_id, // 213494408669063 url can be built in front end
+            createdOn: result.data.content[0].created_at.split(' ')[0], //date survey was created
+            submissionCount: result.data.resultSet.count, // count of total survey submissions
+            results: surveyResults, //array of total true counts
+            ownerID: user.email
+          };
 
-            res.status(200).send(surveyData);
-            console.log('sent surveyData to front-end: ', surveyData);
+          res.status(200).send(surveyData);
+          console.log('sent surveyData to front-end: ', surveyData);
 
-        // If the survey has no submissions yet, send back empty placeholder survey object
+          // If the survey has no submissions yet, send back empty placeholder survey object
         } else {
-            const surveyData = {
-                _id: activeSurvey._id,
-                surveyID: activeSurvey.surveyID,
-                createdOn: String(new Date()).split(' ').splice(1, 3).join('-'),
-                submissionCount: 0,
-                results: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-            }
-            res.status(200).send(surveyData);
-            console.log('no submissions yet, so sent an empty survey to front end: ', surveyData);
+          const surveyData = {
+            _id: activeSurvey._id,
+            surveyID: activeSurvey.surveyID,
+            createdOn: String(new Date()).split(' ').splice(1, 3).join('-'),
+            submissionCount: 0,
+            results: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ownerID: user.email
+          };
+          res.status(200).send(surveyData);
+          console.log('no submissions yet, so sent an empty survey to front end: ', surveyData);
         }
 
-    } catch (e) {
+      } catch (e) {
         console.error(e);
-        res.status(500).send("Server Error");
+        res.status(500).send('Server Error');
+      }
     }
+  });
 }
 
 
