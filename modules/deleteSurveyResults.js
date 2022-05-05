37e637e95ef272d02a56d246dece4469c16c8618
Liesl White <lieslfwhite@gmail.com>
2022-05-04 21:32:13 -0700

    possible survey fixes

--- modules/deleteSurveyResults.js
+++ modules/deleteSurveyResults.js
@@ -4,29 +4,29 @@ const verifyUser = require('../auth');
 const Survey = require("./SurveyModel");
 
 async function handleDeleteSurveyResults(req, res) {
-    console.log('enter handleDeleteSurveyResults');
-    const { id } = req.params;
-    const { email } = req.query;
-    verifyUser(req, async (err, user) => {
+  console.log('enter handleDeleteSurveyResults');
+  const { id } = req.params;
+  const { email } = req.query;
+  verifyUser(req, async (err, user) => {
     if (err) {
-        console.error(err);
-        res.send("Invalid Token");
+      console.error(err);
+      res.send("Invalid Token");
     } else {
-        try {
+      try {
         const survey = await Survey.findOne({ _id: id });
         console.log('found survey: ', survey);
         if (!survey) res.status(400).send("Sorry but you can not do that");
         else {
-            await Survey.findByIdAndDelete(id);
-            console.log('findByIdAndDelete succeeded');
-            res.status(204).send("Bye Bye Answers");
+          await Survey.findByIdAndDelete(id);
+          console.log('findByIdAndDelete succeeded');
+          res.status(204).send("Bye Bye Answers");
         }
-        } catch (e) {
+      } catch (e) {
         console.error(e);
         res.status(500).send("Server Error");
-        }
+      }
     }
-    });
+  });
 }
 
 module.exports = handleDeleteSurveyResults;
